9c93d7326ca9e4f95f16766b32191c3dcf5b42cf
Madden <m-rayner@hotmail.com>
2019-04-05 07:29:45 +1000

    Confirmed working with 6 buttons, resistors and capacitors

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,22 +1,17 @@
-const int btnLeft = 3;
-const int btnRight = 4;
-const int btnUp = 5;
-const int btnDown = 6;
-const int btnA = 7;
-const int btnB = 8;
+#include <Arduino.h>
+#include <HardwareSerial.h>
 
-int btnLeftLast = 0;
-int btnRightLast = 0;
-int btnUpLast = 0;
-int btnDownLast = 0;
-int btnALast = 0;
-int btnBLast = 0;
-int var = 0;
+const int btnLeft = 8;
+const int btnRight = 7;
+const int btnUp = 6;
+const int btnDown = 5;
+const int btnA = 4;
+const int btnB = 3;
 
+int btnLeftLast = 0, btnRightLast = 0, btnUpLast = 0, btnDownLast = 0, btnALast = 0, btnBLast = 0;
 
 void setup() {
 	Serial1.begin(9600); // Bluetooth baud rate
-	// Serial.begin(9600); // Default baud rate
 	pinMode(btnLeft,INPUT);
 	pinMode(btnRight,INPUT);
 	pinMode(btnUp,INPUT);
@@ -33,18 +28,13 @@ void setup() {
 }
 void loop() {
 	if ((digitalRead(btnLeft) != btnLeftLast) || (digitalRead(btnRight) != btnRightLast) || (digitalRead(btnUp) != btnUpLast) || (digitalRead(btnDown) != btnDownLast) || (digitalRead(btnA) != btnALast) || (digitalRead(btnB) != btnBLast)){
-		var++;
-		Serial1.println(var);
-		/* Old code to print digital reads
-			Serial1.print(digitalRead(btnLeft));
-			Serial1.print(digitalRead(btnRight));
-			Serial1.print(digitalRead(btnUp));
-			Serial1.print(digitalRead(btnDown));
-			Serial1.print(digitalRead(btnA));
-			Serial1.println(digitalRead(btnB));
-			*/
-		Serial1.println(digitalRead(btnLeft) + digitalRead(btnRight) + digitalRead(btnUp) + digitalRead(btnDown) + digitalRead(btnA) + digitalRead(btnB));
-
+		Serial1.print(digitalRead(btnLeft));
+		Serial1.print(digitalRead(btnRight));
+		Serial1.print(digitalRead(btnUp));
+		Serial1.print(digitalRead(btnDown));
+		Serial1.print(digitalRead(btnA));
+		Serial1.println(digitalRead(btnB));
+			
 		btnLeftLast = digitalRead(btnLeft);
 		btnRightLast = digitalRead(btnRight);
 		btnUpLast = digitalRead(btnUp);
@@ -52,5 +42,4 @@ void loop() {
 		btnALast = digitalRead(btnA);
 		btnBLast = digitalRead(btnB);
 	}
-	delay(10);
 }
