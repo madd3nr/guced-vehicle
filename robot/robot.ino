6701d535e89ac63b7259afac6beaa1733f98e501
Madden <m-rayner@hotmail.com>
2019-04-06 21:46:50 +1000

    Confirmed outputs working with LEDs in place of motor pins.

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,61 +1,53 @@
-//#include <Servo.h>
-//const int pin_Servo = 6;              // to assign a pin to a Servo.
-
+#include <Arduino.h>
 // for the DC motor terminals
-const int motorL1 = 2;                    // to assign a pin to the INA of a motor.
-const int motorL2 = 3;                    // to assign a pin to the INB of the motor.
+const int motorL1 = 49;                    // to assign a pin to the INA of a motor.
+const int motorL2 = 48;                    // to assign a pin to the INB of the motor.
+const int motorLSpeed = 11;
 
-const int motorR1 = 4;                    // to assign a pin to the INA of a motor.
-const int motorR2 = 5;                    // to assign a pin to the INB of the motor.
-
-//Servo servo;
-/*
-int forward = 0;
-int reverse = 0;
-int left = 0;
-int right = 0;
-int btnA = 0;
-int btnB = 0;
-*/
+const int motorR1 = 47;                    // to assign a pin to the INA of a motor.
+const int motorR2 = 46;                    // to assign a pin to the INB of the motor.
+const int motorRSpeed = 10;
 
 int direction;
 
 void setup() {
 	Serial.begin(9600);
-	Serial1.begin(9600);
 	  
-	pinMode (motorL1, OUTPUT);           // to make the Left motor an actuator.
-	pinMode (motorL2, OUTPUT);           // to make the Left motor an actuator.
-	pinMode (motorR1, OUTPUT);           // to make the Right motor an actuator.
-	pinMode (motorR2, OUTPUT);           // to make the Right motor an actuator.
-
-	//servo.attach(pin_Servo);
+	pinMode(motorL1, OUTPUT);           // to make the Left motor an actuator.
+	pinMode(motorL2, OUTPUT);           // to make the Left motor an actuator.
+	pinMode(motorR1, OUTPUT);           // to make the Right motor an actuator.
+	pinMode(motorR2, OUTPUT);           // to make the Right motor an actuator.
+	pinMode(motorLSpeed, OUTPUT);           // to make the Right motor an actuator.
+	pinMode(motorRSpeed, OUTPUT);           // to make the Right motor an actuator.
 }
-
 void loop() {
 	int dataReceive[6];
-	if (Serial1.available()) {
+	if (Serial.available()) {
+		delay(10);
 		for (int i = 0; i < 6; i++){
-			dataReceive[i] = Serial1.read();
+			dataReceive[i] = Serial.read() - '0';
+			Serial.print(dataReceive[i]);
 		}
+		Serial.print(" - ");
+
 		if (dataReceive[0] == 1){
 			Serial.println("Forward");
-			direction = 1;
+			// direction = 1;
 			moveForward();				// to call the forward function below.				
 		}
 		if (dataReceive[1] == 1){
 			Serial.println("Reverse");
-			direction = 2;
+			// direction = 2;
 			moveBackwards();			// to call the reverse function below.				
 		}
 		if (dataReceive[2] == 1){
 			Serial.println("Left");
-			direction = 3;
+			// direction = 3;
 			turnLeft();					// to call the left function below.				
 		}
 		if (dataReceive[3] == 1){
 			Serial.println("Right");
-			direction = 4;
+			// direction = 4;
 			turnRight();				// to call the right function below.				
 		}
 		if (dataReceive[4] == 1){
@@ -64,12 +56,14 @@ void loop() {
 		if (dataReceive[5] == 1){
 			Serial.println("Button B");
 		}
-		if ((dataReceive[0] == 0) && (dataReceive[1] == 0)){
+		if ((dataReceive[0] == 0) && (dataReceive[1] == 0) && (dataReceive[2] == 0) && (dataReceive[3] == 0)){
+			Serial.println("Stop");
 			stop();
 		}
 	//move();
 	}
 }
+/*
 void move(){
 	switch (direction){
 		case 1: //Forward
@@ -77,12 +71,17 @@ void move(){
 			digitalWrite(motorL2,LOW);
 			digitalWrite(motorR1,HIGH);
 			digitalWrite(motorR2,LOW);
+			digitalWrite(motorLSpeed, HIGH);
+			digitalWrite(motorRSpeed, HIGH);
+			Serial.println("forward");
 			break;
 		case 2: //Reverse
 			digitalWrite(motorL1,LOW);
 			digitalWrite(motorL2,HIGH);
 			digitalWrite(motorR1,LOW);
 			digitalWrite(motorR2,HIGH);
+			digitalWrite(motorLSpeed, HIGH);
+			digitalWrite(motorRSpeed, HIGH);
 			break;
 		case 3: //Left
 			digitalWrite(motorL1,LOW);
@@ -101,39 +100,51 @@ void move(){
 			digitalWrite(motorL2,LOW);
 			digitalWrite(motorR1,LOW);
 			digitalWrite(motorR2,LOW);
+			digitalWrite(motorLSpeed, LOW);
+			digitalWrite(motorRSpeed, LOW);
+
 			break;
 		
 	}
+}*/
+void moveForward() {
+	digitalWrite(motorL1, HIGH);
+	digitalWrite(motorL2, LOW);
+	digitalWrite(motorR1, HIGH);
+	digitalWrite(motorR2, LOW);
+	digitalWrite(motorLSpeed, HIGH);
+	digitalWrite(motorRSpeed, HIGH);
 }
-
-void moveForward(){
-	digitalWrite(motorL1,HIGH);
-	digitalWrite(motorL2,LOW);
-	digitalWrite(motorR1,HIGH);
-	digitalWrite(motorR2,LOW);
+void moveBackwards() {
+	digitalWrite(motorL1, LOW);
+	digitalWrite(motorL2, HIGH);
+	digitalWrite(motorR1, LOW);
+	digitalWrite(motorR2, HIGH);
+	digitalWrite(motorLSpeed, HIGH);
+	digitalWrite(motorRSpeed, HIGH);
 }
-void moveBackwards(){
-	digitalWrite(motorL1,LOW);
-	digitalWrite(motorL2,HIGH);
-	digitalWrite(motorR1,LOW);
-	digitalWrite(motorR2,HIGH);
+void turnLeft() {
+	digitalWrite(motorL1, LOW);
+	digitalWrite(motorL2, HIGH);
+	digitalWrite(motorR1, HIGH);
+	digitalWrite(motorR2, LOW);
+	digitalWrite(motorLSpeed, HIGH);
+	digitalWrite(motorRSpeed, HIGH);
 }
-void turnLeft(){
-	digitalWrite(motorL1,LOW);
-	digitalWrite(motorL2,HIGH);
-	digitalWrite(motorR1,HIGH);
-	digitalWrite(motorR2,LOW);
+void turnRight() {
+	digitalWrite(motorL1, HIGH);
+	digitalWrite(motorL2, LOW);
+	digitalWrite(motorR1, LOW);
+	digitalWrite(motorR2, HIGH);
+	digitalWrite(motorLSpeed, HIGH);
+	digitalWrite(motorRSpeed, HIGH);
 }
-void turnRight(){
-	digitalWrite(motorL1,HIGH);
-	digitalWrite(motorL2,LOW);
-	digitalWrite(motorR1,LOW);
-	digitalWrite(motorR2,HIGH);
-}
-void stop(){
-	digitalWrite(motorL1,LOW);
-	digitalWrite(motorL2,LOW);
-	digitalWrite(motorR1,LOW);
-	digitalWrite(motorR2,LOW);
+void stop() {
+	digitalWrite(motorL1, LOW);
+	digitalWrite(motorL2, LOW);
+	digitalWrite(motorR1, LOW);
+	digitalWrite(motorR2, LOW);
+	digitalWrite(motorLSpeed, LOW);
+	digitalWrite(motorRSpeed, LOW);
 	delay(500);
 }
