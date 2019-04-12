3a6183cf6cc3a52a5480780df50239cdba6ded86
Madden <m-rayner@hotmail.com>
2019-04-12 19:46:37 +1000

    change to using bits for transmit

--- controller/controller.ino
+++ controller/controller.ino
@@ -8,10 +8,29 @@ const int btnDown = 5;
 const int btnA = 4;
 const int btnB = 3;
 
+typedef struct {
+	unsigned int bit0:1;
+	unsigned int bitLeft:1;
+	unsigned int bitRight:1;
+	unsigned int bitUp:1;
+	unsigned int bitDown:1;
+	unsigned int bitA:1;
+	unsigned int bitB:1;
+	unsigned int bit7:1;
+} creation;
+
+union otherCreation {
+	unsigned int packet;
+	creation bits;
+};
+
+otherCreation buttons;
+
 int btnLeftLast = 0, btnRightLast = 0, btnUpLast = 0, btnDownLast = 0, btnALast = 0, btnBLast = 0;
 
 void setup() {
 	Serial.begin(9600); // Bluetooth baud rate
+	buttons.bits.bit7 = 1;
 	pinMode(btnLeft,INPUT);
 	pinMode(btnRight,INPUT);
 	pinMode(btnUp,INPUT);
@@ -21,25 +40,28 @@ void setup() {
 	while (!Serial) {
 		; // wait for serial port to connect. Needed for native USB
 	}
-	Serial.write(27);
-	Serial.print("[2J");
-	Serial.write(27);
-	Serial.print("[H");
+	//Serial.write(27);
+	//Serial.print("[2J");
+	//Serial.write(27);
+	//Serial.print("[H");
 }
 void loop() {
 	if ((digitalRead(btnLeft) != btnLeftLast) || (digitalRead(btnRight) != btnRightLast) || (digitalRead(btnUp) != btnUpLast) || (digitalRead(btnDown) != btnDownLast) || (digitalRead(btnA) != btnALast) || (digitalRead(btnB) != btnBLast)){
+		/*
 		Serial.print(digitalRead(btnLeft));
 		Serial.print(digitalRead(btnRight));
 		Serial.print(digitalRead(btnUp));
 		Serial.print(digitalRead(btnDown));
 		Serial.print(digitalRead(btnA));
 		Serial.println(digitalRead(btnB));
-			
-		btnLeftLast = digitalRead(btnLeft);
-		btnRightLast = digitalRead(btnRight);
-		btnUpLast = digitalRead(btnUp);
-		btnDownLast = digitalRead(btnDown);
-		btnALast = digitalRead(btnA);
-		btnBLast = digitalRead(btnB);
+		*/
+		btnUpLast = buttons.bits.bitUp = digitalRead(btnUp);
+		btnDownLast = buttons.bits.bitDown = digitalRead(btnDown);
+		btnLeftLast = buttons.bits.bitLeft = digitalRead(btnLeft);
+		btnRightLast = buttons.bits.bitRight = digitalRead(btnRight);
+		btnALast = buttons.bits.bitA = digitalRead(btnA);
+		btnBLast = buttons.bits.bitB = digitalRead(btnB);
+
+		Serial.print(buttons.packet);
 	}
 }
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 // for the DC motor terminals
-const int motorL1 = 49;                    // to assign a pin to the INA of a motor.
-const int motorL2 = 48;                    // to assign a pin to the INB of the motor.
+const int motorL1 = 49;			// to assign a pin to the INA of a motor.
+const int motorL2 = 48;			// to assign a pin to the INB of the motor.
 const int motorLSpeed = 11;
 
-const int motorR1 = 47;                    // to assign a pin to the INA of a motor.
-const int motorR2 = 46;                    // to assign a pin to the INB of the motor.
+const int motorR1 = 46;			// to assign a pin to the INA of a motor.
+const int motorR2 = 47;			// to assign a pin to the INB of the motor.
 const int motorRSpeed = 10;
 
 int direction;
 
+typedef struct {
+	unsigned int bit0:1;
+	unsigned int bitLeft:1;
+	unsigned int bitRight:1;
+	unsigned int bitUp:1;
+	unsigned int bitDown:1;
+	unsigned int bitA:1;
+	unsigned int bitB:1;
+	unsigned int bit7:1;
+} creation;
+
+union otherCreation {
+	unsigned int packet;
+	creation bits;
+};
+
+otherCreation buttons;
+
 void setup() {
 	Serial.begin(9600);
-	  
+	Serial3.begin(9600);
+	buttons.packet = 0;
 	pinMode(motorL1, OUTPUT);           // to make the Left motor an actuator.
 	pinMode(motorL2, OUTPUT);           // to make the Left motor an actuator.
 	pinMode(motorR1, OUTPUT);           // to make the Right motor an actuator.
@@ -21,43 +40,45 @@ void setup() {
 	pinMode(motorRSpeed, OUTPUT);           // to make the Right motor an actuator.
 }
 void loop() {
-	int dataReceive[6];
 	if (Serial.available()) {
 		delay(10);
-		for (int i = 0; i < 6; i++){
-			dataReceive[i] = Serial.read() - '0';
-			Serial.print(dataReceive[i]);
+		int pass = 1;
+		while(pass) {
+			buttons.packet = Serial.read();
+			Serial3.println(buttons.packet);
+			if (buttons.bits.bit7 == 1) {
+				pass = 0;
+			}
 		}
-		Serial.print(" - ");
 
-		if (dataReceive[0] == 1){
-			Serial.println("Forward");
+		if (buttons.bits.bitUp == 1){
+			Serial3.println("Forward");
 			// direction = 1;
 			moveForward();				// to call the forward function below.				
 		}
-		if (dataReceive[1] == 1){
-			Serial.println("Reverse");
+		if (buttons.bits.bitDown == 1){
+			Serial3.println("Reverse");
 			// direction = 2;
 			moveBackwards();			// to call the reverse function below.				
 		}
-		if (dataReceive[2] == 1){
-			Serial.println("Left");
+		if (buttons.bits.bitLeft == 1){
+			Serial3.println("Left");
 			// direction = 3;
 			turnLeft();					// to call the left function below.				
 		}
-		if (dataReceive[3] == 1){
-			Serial.println("Right");
+		if (buttons.bits.bitRight == 1){
+			Serial3.println("Right");
 			// direction = 4;
 			turnRight();				// to call the right function below.				
 		}
-		if (dataReceive[4] == 1){
-			Serial.println("Button A");
+		if (buttons.bits.bitA == 1){
+			Serial3.println("Button A");
 		}
-		if (dataReceive[5] == 1){
-			Serial.println("Button B");
+		if (buttons.bits.bitB == 1){
+			Serial3.println("Button B");
 		}
-		if ((dataReceive[0] == 0) && (dataReceive[1] == 0) && (dataReceive[2] == 0) && (dataReceive[3] == 0)){
-			Serial.println("Stop");
+		if ((buttons.bits.bitUp == 0) && (buttons.bits.bitDown == 0) && (buttons.bits.bitLeft == 0) && (buttons.bits.bitRight == 0)){
+			Serial3.println("Stop");
 			stop();
 		}
 	//move();
